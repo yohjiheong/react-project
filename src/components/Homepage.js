5f33e6a57cc257fe1ff48f8bc9b69d726f8efb18
yohji <yohjiheonggg@gmail.com>
2021-12-29 20:30:56 +0800

    debug

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -2,21 +2,10 @@ import React from 'react';
 
 function Homepage() {
 
-    var i = 0;
-    var txt = 'Welcome to Crossfit!';
-    var speed = 50; /* The speed/duration of the effect in milliseconds */
-
-    var textEffect = () => {
-        if (i < txt.length) {
-            document.getElementById("textEffect").innerHTML += txt.charAt(i);
-            i++;
-            setTimeout(textEffect, speed);
-        }
-    }
     
     return (
         <div className='homepage'>
-            <h1 id="textEffect" align="center" > </h1>
+            <h1 align="center"> Welcome to Crossfit! </h1>
             <p> Your personal fitness tracker! </p>
         </div>
     );
